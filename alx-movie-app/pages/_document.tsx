b51f4321fe530241d8180b24091b51d394a0d063
SylvesterK-code <sylvesterkeystarling@gmail.com>
2025-08-25 00:08:57 +0000

    2. Create a Web App Manifest

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,10 +1,51 @@
+// import { Html, Head, Main, NextScript } from "next/document";
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body className="antialiased">
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Favicon fallback */}
+        <link rel="icon" href="/icons/android-chrome-192x192.png" />
+
+        {/* Theme color */}
+        <meta name="theme-color" content="#FFFFFF" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
